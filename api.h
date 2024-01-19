72c7a0b5b2f86fece07ab9d1cae48f9b95d3e4db
seeeagull <yuzhujiang0324@gmail.com>
2024-01-19 15:08:44 +0800

    small changes for symbols

--- api.h
+++ api.h
@@ -2,20 +2,20 @@
 
 #include <vector>
 
-#ifndef EXPORT
+#ifndef INSTANTMESH_EXPORT
 #  if defined(_MSC_VER) || defined(__CYGWIN__)
 #    ifdef InstantMesh_EXPORT
-#      define EXPORT __declspec(dllexport)
+#      define INSTANTMESH_EXPORT __declspec(dllexport)
 #    else
-#      define EXPORT __declspec(dllimport)
+#      define INSTANTMESH_EXPORT __declspec(dllimport)
 #    endif
 #  elif defined(__clang__) || defined(__GNUC__)
-#    define EXPORT __attribute__((visibility("default")))
+#    define INSTANTMESH_EXPORT __attribute__((visibility("default")))
 #  endif
 #endif
 
 
-EXPORT int runInstantMeshes(std::vector<std::vector<int>> &faces,
+INSTANTMESH_EXPORT int runInstantMeshes(std::vector<std::vector<int>> &faces,
                             std::vector<std::vector<float>> &verts,
                             const std::vector<std::vector<int>> &features,
                             int argc, char **argv);
